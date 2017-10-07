c1adb252c9ac942fdfaa21b103aa143fb9a22416
Chris Meyers <chris.meyers.fsu@gmail.com>
2017-10-07 12:34:35 -0400

    remove any ansible ssh connection

--- action_plugins/docker_provision.py
+++ action_plugins/docker_provision.py
@@ -6,37 +6,6 @@ import sys
 from ansible.plugins.action import ActionBase
 
 class ActionModule(ActionBase):
-
-    # 1. private.ansible_ssh_user
-    # 2. docker_container pass through via 'user'
-    # 3. 'root'
-    def get_ansible_ssh_user(self, task_vars):
-        return task_vars.get('private', {}).get('ansible_ssh_user') or \
-                task_vars.get('user', 'root')
-
-    # TODO: Currently only supports finding the ip in bridge
-    def get_ansible_ssh_host(self, docker_container_facts):
-        return docker_container_facts.get('NetworkSettings', {}) \
-                                     .get('Networks', {}) \
-                                     .get('bridge', {}) \
-                                     .get('IPAddress')
-
-    # 1. private.ansible_ssh_pass
-    # 2. 'docker.io'
-    def get_ansible_ssh_pass(self, task_vars):
-        return task_vars.get('private', {}).get('ansible_ssh_pass') or \
-                'docker.io'
-
-    # TODO: Only ssh building supported
-    def build_ansible_connection_ssh(self, task_vars, docker_container_facts):
-        v = dict()
-        v['ansible_ssh_user'] = self.get_ansible_ssh_user(task_vars)
-        v['ansible_ssh_pass'] = self.get_ansible_ssh_pass(task_vars)
-        ansible_ssh_host = self.get_ansible_ssh_host(docker_container_facts)
-        if ansible_ssh_host:
-            v['ansible_ssh_host'] = ansible_ssh_host
-        return v
-
     def run(self, tmp=None, task_vars=None):
 
         self._supports_check_mode = False
